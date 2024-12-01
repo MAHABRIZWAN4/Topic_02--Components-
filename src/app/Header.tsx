bea02cdaf8d565849e5f5814402d97427492e42a
Mahab Rizwan <mahabrizwan@example.com>
2024-12-01 18:34:35 +0500

    main

---
+++ src/app/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header(){
+    return (
+        <div>
+            <ul className="bg-red-600 text-white font-extrabold text-xl flex h-16 gap-16 pl-4 items-center ">
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Contact-Us</li>
+
+            </ul>
+        </div>
+    )
+}
